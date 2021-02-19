7ad77b15bed191b8a98dd6a2934fe3f6c4e261d6
Dan Alvizu <dalvizu@pingidentity.com>
2021-02-19 13:52:29 -0700

    port to junit and fix tests

--- src/test/java/com/newrelic/experts/jenkins/EventRecorderImplTest.java
+++ src/test/java/com/newrelic/experts/jenkins/EventRecorderImplTest.java
@@ -6,24 +6,36 @@
 package com.newrelic.experts.jenkins;
 
 import com.newrelic.experts.client.model.Event;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import hudson.model.Computer;
+import hudson.model.Label;
+import hudson.model.labels.LabelAtom;
+import hudson.util.TagCloud;
+import jenkins.model.Jenkins;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 
 /**
  * @author dalvizu
  */
-@Test(groups = { "unit" })
 public class EventRecorderImplTest {
 
   private EventRecorderImpl eventRecorder;
 
-  @BeforeMethod
+  @Before
   public void setup() {
     eventRecorder = new EventRecorderImpl();
   }
@@ -44,13 +56,47 @@ public class EventRecorderImplTest {
   }
 
   @Test
-  public void testRecordJenkinsMasterEvent() {
-    eventRecorder.recordJenkinsMasterEvent(true, 10);
-    List<Event> results = Arrays.asList(eventRecorder.popEvents());
-    assertEquals(1, results.size());
-    assertEquals("", results.get(0).get(Event.PROPERTY_NAME_EVENT_TYPE));
-    assertEquals(10.0f, results.get(0).get("agentConnectedCount"));
-    assertEquals(1.0f, results.get(0).get("quietDownMode"));
+  public void testRecordJenkinsMasterEvent() throws Exception {
+    try(MockedStatic<Jenkins> theMock = Mockito.mockStatic(Jenkins.class)) {
+      Jenkins mockJenkins = mock(Jenkins.class);
+      theMock.when(Jenkins::getInstance).thenReturn(mockJenkins);
+      theMock.when(Jenkins::get).thenReturn(mockJenkins);
+      mockHostname(mockJenkins, "mock hostname");
+      mockLabels(mockJenkins, "corp it");
+      eventRecorder.recordJenkinsMasterEvent(true, 10);
+      List<Event> results = Arrays.asList(eventRecorder.popEvents());
+      assertEquals(1, results.size());
+      assertEquals("JenkinsMasterEvent", results.get(0).get(Event.PROPERTY_NAME_EVENT_TYPE));
+      assertEquals(10.0f, results.get(0).get("agentConnectedCount"));
+      assertEquals(1.0f, results.get(0).get("quietDownMode"));
+      assertEquals("corp|it", results.get(0).get("jenkinsMasterLabels"));
+      assertEquals("mock hostname", results.get(0).get("jenkinsMasterHost"));
+    }
   }
 
+  private void mockHostname(Jenkins mockJenkins, String hostname)
+    throws IOException, InterruptedException {
+    Computer computer = mock(Computer.class);
+    when(mockJenkins.toComputer()).thenReturn(computer);
+    when(computer.getHostName()).thenReturn(hostname);
+  }
+
+  /**
+   * Mock the labels to add on recorded events
+   *
+   * @param mockJenkins - the Jenkins object to mock
+   * @param labelString - a whitespace separated list of labels describing the
+   *                    master
+   *                    (Jenkins convention)
+   */
+  private void mockLabels(Jenkins mockJenkins, String labelString) {
+    when(mockJenkins.getLabelAtom(anyString())).thenAnswer( I -> new LabelAtom(I.getArgument(0)) );
+    Set<LabelAtom> labels = Label.parse(labelString);
+    TagCloud<LabelAtom> tagCloud =  new TagCloud<>(labels, new TagCloud.WeightFunction<LabelAtom>() {
+      public float weight(LabelAtom item) {
+        return 1;
+      }
+    });
+    when(mockJenkins.getLabelCloud()).thenReturn(tagCloud);
+  }
 }
